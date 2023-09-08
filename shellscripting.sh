6a69485211b27a92f4023381d2b710f20b636cc8
preetikg <preetikg2008@gmail.com>
2023-09-07 23:23:38 -0400

    checkin file of number

--- shellscripting.sh
+++ shellscripting.sh
@@ -1,5 +1,15 @@
 # !/bin/bash
-echo "my current directory is $(pwd)"
+#echo "my current directory is $(pwd)"
+#cd /home/centos
+#echo "my current directory is $(pwd)"
+#echo "numbers of file $(ls | wc -l)"
 cd /home/centos
 echo "my current directory is $(pwd)"
-echo "numbers of file $(ls | wc -l)"
+echo "i am creatint a new directory abcdir"
+mkdir abcdir
+cd abcdir
+echo "now i am in $(pwd)"
+echo "creating a new file name"
+touch name
+echo "my name is preeti > name"
+echo "the content of file is: $(cat name)"   
