ff90eeb122a57064922b9d4f7a5847f050dca693
preetikg <preetikg2008@gmail.com>
2023-09-09 01:24:46 -0400

    checkin file of number

--- jenkins/jenkinsinstall.sh
+++ jenkins/jenkinsinstall.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-echo " I am going to install java"
- sudo apt update && sudo apt install default-jre && java -version
-sudo apt install default-jdk && javac -version
+echo " I am going to instal
+sudo apt install default-jdk && jal java"
+ sudo apt update && sudo apt install default-jre && java -versionvac -version
 echo " I am goin to install jenkins"
 touch jenkins-log
  wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add - > jenkins-log
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list' >> jenkins-log
  sudo apt update >> jenkins-log
  sudo apt install jenkins >> jenkins-log
+ echo " Staring jenkinsand checking status"
  sudo systemctl start jenkins && sudo systemctl status jenkins 
                           
---
+++ webserverinstallation/httpdinstall.sh
@@ -0,0 +1,2 @@
+echo " I am going to install httpd web server to host web application"
+sudo apt install appach2 -y
