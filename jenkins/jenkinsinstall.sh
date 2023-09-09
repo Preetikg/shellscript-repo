96dbdbdac8079e29f145369808b8862f01964ba2
preetikg <preetikg2008@gmail.com>
2023-09-09 00:19:16 -0400

    checkin file of number

--- jenkins/jenkinsinstall.sh
+++ jenkins/jenkinsinstall.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-mkdir /home/centos/logs
-touch /home/centos/logs/javalogs
-echo " i am installing java" > /home/centos/logs/javalogs
-echo "Performing Yum update" >> /home/centos/logs/javalogs
-echo "-----------------------------------">> /home/centos/logs/javalogs
-sudo yum update -y >> /home/centos/logs/javalogs
-echo "Performing Installing Java ">> /home/centos/logs/javalogs
-echo "-----------------------------------">> /home/centos/logs/javalogs
-sudo yum install java-11-openjdk-devel -y >> /home/centos/logs/javalogs
+echo " I am going to install java"
+sudo apt install default java -jdk 
+                          
